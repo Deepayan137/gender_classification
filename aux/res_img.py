c337bd7fd04fca9856f16285c445bb47d29e1956
vamsidharmuthireddy <Bat2dark>
2017-11-01 16:59:07 +0530

    added_res_img

--- aux/res_img.py
+++ aux/res_img.py
@@ -4,16 +4,16 @@ import numpy as np
 
 def downscale(input, rows=256, cols=256):
     input = np.array(input)
-    scale_x = rows/input.shape[0]
-    scale_y = rows/input.shape[1]
+    scale_x = cols/input.shape[1]
+    scale_y = rows/input.shape[0]
     final = cv2.resize(input, None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_AREA)
     return final
 
 
 def upcale(input, rows=256, cols=256):
     input = np.array(input)
-    scale_x = rows/input.shape[0]
-    scale_y = rows/input.shape[1]
+    scale_x = cols/input.shape[1]
+    scale_y = rows/input.shape[0]
     final = cv2.resize(input, None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_CUBIC)
     
     return final
