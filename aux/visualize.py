29a0f4b2947efdd88b920400af7794cc28235dba
Deepayan <deepayan137@gmail.com>
2017-11-11 16:00:06 +0530

    Results and stats for method1

--- aux/visualize.py
+++ aux/visualize.py
@@ -1,12 +1,31 @@
 from matplotlib import pyplot as plt 
+import cv2
+def plot(images, **kwargs):
 
-def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
-    """Helper function to plot a gallery of portraits"""
-    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
-    for i in range(n_row * n_col):
-        plt.subplot(n_row, n_col, i + 1)
-        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
-        plt.title(titles[i], size=12)
-        plt.xticks(())
-        plt.yticks(())
+	images = [cv2.resize(images[i], (100, 100), interpolation=cv2.INTER_CUBIC) for i in range(len(images))]
+	outfile = kwargs["output"]
+
+	if "predictions" in kwargs:
+		# print(predict)
+		predict = kwargs["predictions"]
+		predict = ['female' if predict[i] == '1' else 'male' for i in range(len(predict))]
+	for i,eig in enumerate(images[:6]):
+		if i<3:
+			plt.subplot(1,3,i+1), plt.imshow(eig, cmap=plt.cm.gray), plt.xticks([]), plt.yticks([])
+			if "predictions" in kwargs:
+				plt.title(predict[i])
+		if i<6:
+			plt.subplot(2,3,i+1), plt.imshow(eig, cmap=plt.cm.gray), plt.xticks([]), plt.yticks([])
+			if "predictions" in kwargs:
+				plt.title(predict[i])
+		plt.savefig(outfile)
+
+def plot_stats(df):
+	x = (df['n_components'])
+	plt.plot(x, df['Accuracy'], color='r', label='Accuracy')
+	plt.plot(x, df['F1-score'], color='b', label='F1-score')
+	plt.legend()
+	plt.title("Accuracy, F1-score vs. n_components")
+	plt.plot()
+	plt.show()
+	plt.savefig('stats.png')
--- test.py
+++ test.py
@@ -13,6 +13,9 @@ from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_sc
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from matplotlib import pyplot as plt
+from aux.visualize import plot, plot_stats
+from random import shuffle
+import pandas as pd
 if __name__ == '__main__':
 	config = json.load(open(sys.argv[1]))
 	image_locs = list(map(lambda x: config["dir"] + x  , os.listdir(config["dir"])))
@@ -21,30 +24,34 @@ if __name__ == '__main__':
 	pr = Preprocess(haar_path=config["haar_path"])
 	norm = np.array([pr.normalize(image) for image in images])
 	norm = norm.reshape(100, -1)
-	n_comp = 20
-	
-	X_train, X_test, y_train, y_test = train_test_split(norm, truths, test_size=0.40, random_state=42)
-	X_train_pca, X_test_pca, eigenfaces = pca_decmp(X_train, X_test, n_comp)
-	print(eigenfaces[0].shape)
-	plt.imshow(eigenfaces[0], cmap=plt.cm.gray)
-	plt.show()
-	#cv2.waitKey(0)
-	# pdb.set_trace()
-	# print("Fitting the classifier to the training set")
-	# t0 = time()
-	# clf = 	SVC(kernel='rbf', class_weight='balanced')
-	# clf = clf.fit(X_train_pca, y_train)
-	# print("done in %0.3fs" % (time() - t0))
-	# print("Best estimator found by grid search:")
-	
-	# print("Predicting people's names on the test set")
-	# t0 = time()
-	# y_pred = clf.predict(X_test_pca)
-	# print(accuracy_score(y_test, y_pred))
-	# pr = precision_score(y_test, y_pred, average='weighted')
-	# re = recall_score(y_test, y_pred, average='weighted')
-	# f1 = 2*(pr*re)/float((pr+re))
-	# print('f1-score: %.2f'%f1)
-	# print("done in %0.3fs" % (time() - t0))
+	n_comp = [20, 25, 35, 45, 60]
+	row = []
+	for comp in n_comp:
+		X_train, X_test, y_train, y_test = train_test_split(norm, truths, test_size=0.20, random_state=42, stratify=truths)
+		X_train_pca, X_test_pca, eigenfaces = pca_decmp(X_train, X_test, comp)
 
-	
+		# plot(eigenfaces, output="eigen.png")
+		print("Fitting the classifier to the training set")
+		t0 = time()
+		clf = 	SVC(kernel='rbf', class_weight='balanced')
+		clf = clf.fit(X_train_pca, y_train)
+		print("done in %0.3fs" % (time() - t0))
+		print("Best estimator found by grid search:")
+		
+		print("Predicting people's names on the test set")
+		t0 = time()
+		y_pred = clf.predict(X_test_pca)
+		sh = list(range(len(X_test)))
+		shuffle(sh)
+		X_test = [np.reshape(X_test[sh[i]],(48,48)) for i in range(len(sh))]
+		y_ = [y_pred[sh[i]] for i in range(len(sh))]
+		# plot(X_test, output="predictions.png", predictions=y_)
+		acc = (accuracy_score(y_test, y_pred))
+		pr = precision_score(y_test, y_pred, average='weighted')
+		re = recall_score(y_test, y_pred, average='weighted')
+		f1 = 2*(pr*re)/float((pr+re))
+		print('f1-score: %.2f'%f1)
+		print("done in %0.3fs" % (time() - t0))
+		row.append([comp, acc, f1])
+	df = pd.DataFrame(row, columns=['n_components', 'Accuracy', 'F1-score'])
+	plot_stats(df)
