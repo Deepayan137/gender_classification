52d27e0b8ae8f852ff4e1cd2ffd9bd01aeb5ba2f
Ashutosh <ashchamp1995@gmail.com>
2017-11-11 17:00:41 +0530

    added knn

--- test_knn.py
+++ test_knn.py
@@ -3,7 +3,6 @@ import sys
 from aux.data_loader import images_and_truths, pair_to_unit
 from pre.preproc import Preprocess
 from pre.dim_reduce import pca_decmp
-#from sklearn.model_selection import train_test_split
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import StratifiedShuffleSplit
 import numpy as np
@@ -15,6 +14,7 @@ from time import time
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import GridSearchCV
 from sklearn import linear_model
+from sklearn.neighbors import KNeighborsClassifier
 acc_score = 0
 if __name__ == '__main__':
 	config = json.load(open(sys.argv[1]))
@@ -26,13 +26,17 @@ if __name__ == '__main__':
 	norm = norm.reshape(100, -1)
 	X_train, X_test, y_train, y_test = train_test_split(norm, truths, test_size=0.4, random_state=42, stratify=truths)
 	X_train_pca, X_test_pca = pca_decmp(X_train, X_test)
-	penalty = ['l1','l2']
-	param_grid = {'C': [0.00001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 100000]}
-	clf = GridSearchCV(linear_model.LogisticRegression(penalty=penalty[1], class_weight='balanced'), param_grid)
-	clf = clf.fit(X_train_pca,y_train)
-	y_pred = clf.predict(X_test_pca) 
-	print(metrics.classification_report(y_test, y_pred))
-	print(metrics.confusion_matrix(y_test, y_pred))
-	acc_score = accuracy_score(y_test,y_pred)
-	print("The accuracy %f with Logistic Regression having penalty %s",(acc_score,penalty[1]))
- 	       
+	for i in range(10):
+            if i == 0:
+                count = 1
+	    else:
+		metric = ['minkowski', 'euclidean']
+	        model = KNeighborsClassifier(n_neighbors=i, 
+	                weights='uniform', algorithm='auto', leaf_size=30, p=2, metric=metric[1], metric_params=None, n_jobs=1)
+	        model.fit(X_train_pca, y_train)
+	        y_pred = model.predict(X_test_pca)
+	        acc_score = accuracy_score(y_test, y_pred)*100
+	        print(" Accuracy for KNN with NN: %d and metric %s is %f" % (i, metric[1], acc_score))
+ 	   
+
+
